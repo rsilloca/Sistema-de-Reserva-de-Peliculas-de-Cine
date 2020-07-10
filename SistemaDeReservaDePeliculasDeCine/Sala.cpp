e52ec366c44039c35ce9b689688b0dca2b8c4e1d
MelanyRios <mriosca@unsa.edu.pe>
2020-07-09 21:59:49 -0500

    Cambios atributos melany

--- SistemaDeReservaDePeliculasDeCine/Sala.cpp
+++ SistemaDeReservaDePeliculasDeCine/Sala.cpp
@@ -1,2 +1,28 @@
 #include "pch.h"
 #include "Sala.h"
+
+int Sala::getId(){
+	return id ;
+}
+void Sala::setId(int Id) {
+	id = Id;
+}
+int Sala::getCapacidad() {
+	return capacidad;
+}
+void Sala::setCapacidad(int capacity) {
+	capacidad = capacity;
+}
+TipoSala Sala::getTipo() {
+	return tipo;
+}
+void Sala::setTipo(TipoSala type) {
+	tipo = type;
+}
+Butaca** Sala::getButacas() {
+	return butacas ;
+}
+
+void Sala::setButacas (Butaca** armchairs) {
+	butacas = armchairs;
+}
