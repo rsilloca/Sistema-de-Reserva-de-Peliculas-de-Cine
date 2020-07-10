a035771fed49cd4dd292083fa81aea9d471b99a6
ShokamiNeo <amolloa@unsa.edu.pe>
2020-07-10 11:04:13 -0500

    Atributos, gets y sets (1):
    - CategoriaPelicula
    - Fecha
    - FuncionDeCine
    - Hora
    - Pelicula

--- SistemaDeReservaDePeliculasDeCine/FuncionDeCine.h
+++ SistemaDeReservaDePeliculasDeCine/FuncionDeCine.h
@@ -1,5 +1,22 @@
 #pragma once
+#include "Fecha.h"
+#include "Pelicula.h"
 class FuncionDeCine
 {
+private:
+	Pelicula pelicula;
+	Fecha fecha;
+	int numeroSala;
+	int id;
+
+public:
+	void setPelicula(Pelicula movie);
+	void setFecha(Fecha date);
+	void setNumeroSala(int hallNumber);
+	void setId(int idNumber);
+	Pelicula getPelicula();
+	Fecha getFecha();
+	int getNumeroSala();
+	int getId();
 };
 
--- SistemaDeReservaDePeliculasDeCine/Hora.h
+++ SistemaDeReservaDePeliculasDeCine/Hora.h
@@ -1,5 +1,17 @@
 #pragma once
 class Hora
 {
+private:
+	int horas;
+	int minutos;
+	int segundos;
+
+public:
+	void setHoras(int hour);
+	void setMinutos(int minute);
+	void setSegundos(int seconds);
+	int getHoras();
+	int getMinutos();
+	int getSegundos();
 };
 
